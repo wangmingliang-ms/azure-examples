11c297e318fe31a2d36eec46089c3933cf79b238
Hanxiao Liu <hanli@microsoft.com>
2022-08-16 13:56:22 +0800

    Add examples for cosmos db account resource connector

---
+++ example-connector-cosmos-sql-spring/src/main/java/com/microsoft/azure/samples/spring/Pet.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.samples.spring;
+
+import com.azure.spring.data.cosmos.core.mapping.Container;
+import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
+import org.springframework.data.annotation.Id;
+
+import java.util.UUID;
+
+@Container(containerName = "pet-test")
+public class Pet {
+    @Id
+    private UUID id;
+    @PartitionKey
+    private String name;
+
+    private String species;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", species='" + species + '\'' +
+                '}';
+    }
+}
